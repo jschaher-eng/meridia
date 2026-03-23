088d08ae3d9cdab2ca01e9b5920eb6ad99dcc84f
jschaher-eng <jschaher@gmail.com>
2026-03-23 19:53:37 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -59,7 +59,7 @@ function dashTab(t) {
   var sm = document.getElementById('sm-' + t);
   if (sm) sm.classList.add('act');
   if (t === 'dossier') { loadDashboard(); }
-  if (t === 'messages') { loadMessages(); }
+  if (t === 'messages') { loadClientMessages(); }
   window.scrollTo(0, 0);
 }
 
